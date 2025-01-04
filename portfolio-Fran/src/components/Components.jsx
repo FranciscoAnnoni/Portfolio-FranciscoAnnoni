fa7d45d41c1cf3e020139e743750417c0c9cde28
franciscoannoni <franciscoannoni@gmail.com>
2025-01-04 15:47:36 -0300

    work section olmost finisht

--- portfolio-Fran/src/components/Components.jsx
+++ portfolio-Fran/src/components/Components.jsx
@@ -187,10 +187,10 @@ export const ProjectCard = ({ title, description, technologies, githubLink, proj
                       <IoFolderOutline/>
                       </div>
                       <div className="project-links">
-                        <a href="https://github.com/bchiang7/octoprofile" aria-label="GitHub Link" target="_blank" rel="noopener noreferrer">
+                        <a href={githubLink} aria-label="GitHub Link" target="_blank" rel="noopener noreferrer">
                           <FaGithub/>
                         </a>
-                        <a href="https://octoprofile.now.sh" aria-label="External Link" className="external" target="_blank" rel="noopener noreferrer">
+                        <a href={projectLink} aria-label="External Link" className="external" target="_blank" rel="noopener noreferrer">
                           <FiExternalLink/>
                         </a>
                       </div>
@@ -214,3 +214,50 @@ export const ProjectCard = ({ title, description, technologies, githubLink, proj
   );
 };
 
+
+export const FeaturedProjectCard = ({ title, description, technologies, githubLink, projectLink }) => {
+  return (
+    <li className="featured__StyledProject-ywnbqt-1 gvznfB" data-sr-id="4" style={{ visibility: 'visible', opacity: 1, transform: 'matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)', transition: 'all, opacity 0.5s cubic-bezier(0.645, 0.045, 0.355, 1), transform 0.5s cubic-bezier(0.645, 0.045, 0.355, 1)' }}>
+      <div className="project-content">
+        <div>
+          <p className="project-overline">Featured Project</p>
+          <h3 className="project-title">
+            <a href={projectLink} rel="noopener noreferrer" target="_blank">{title}</a>
+          </h3>
+          <div className="project-description">
+            <p>{description}</p>
+          </div>
+          <ul className="project-tech-list">
+            {technologies.map((tech, index) => (
+              <li key={index}>{tech}</li>
+            ))}
+          </ul>
+          <div className="project-links">
+            <a href={githubLink} aria-label="GitHub Link" rel="noopener noreferrer" target="_blank">
+              <FaGithub />
+            </a>
+            <a href={projectLink} aria-label="External Link" className="external" rel="noopener noreferrer" target="_blank">
+              <FiExternalLink />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="project-image">
+        <a href={projectLink} rel="noopener noreferrer" target="_blank">
+          <div data-gatsby-image-wrapper="" className="gatsby-image-wrapper gatsby-image-wrapper-constrained img">
+            <div style={{ maxWidth: '700px', display: 'block' }}>
+              <img alt="" role="presentation" aria-hidden="true" src="data:image/svg+xml;charset=utf-8,%3Csvg height='438' width='700' xmlns='http://www.w3.org/2000/svg' version='1.1'%3E%3C/svg%3E" style={{ maxWidth: '100%', display: 'block', position: 'static' }} />
+            </div>
+            <img aria-hidden="true" data-placeholder-image="" decoding="async" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAANCAIAAAAmMtkJAAAACXBIWXMAAAsTAAALEwEAmpwYAAACBklEQVQoz23O7W6bMBQGYO5iSoEY7GBjY2MwH/6AJk3SrStVlQbabJq23f9lTCRR1x+VHllH57zHtie76VOZmbidhDuf9vOMl7vxgyl3k7Bj2b/uH093D6dm89rfv3X7N2Gny/Rj3hP2+J+Zcf2iuvHv7x/jdHo5vv359fM4nrL2Ov2Y94R5eSfdUXZj4UbpRqaP3BxzN3Izk+4qn9+45j2uD1wfhDlwc5BuLLqp6Maz6XLLpX4vcnvk5zA3B4+3z0I/0XpgzROrh7Sa0Xq4dLL2Oa0er+qBzc2B1XOeNoPH6iHhOiblTSQT0SNuAFE+Uj4sfVjG1CTCkWIdEwNQuYjLm7j0oQqQiqjziHpIWJPr9ZI6IDa0vaftGkoXi36ZuiXRMe9w+ZVXD7zdFlUPM+OjClJDilsvVd8xazBvF7EE1LF6l5s9re6R3GK5BakNsUGpw/ldVN5JZRKpfVQBXENmPVJ+A6hEpFlEwk8aLG+pcii3ATbBqvGhilKbFDuidqxaA96F2IH5RyYkxlvJPYhFKjdLogOssVpz2ydNH3MN0yZMKjAvb0W9E/WGlBvIbudNrENqvVW+A7Egol/AMsQaFWskWsRNIjrITLCqQGri1BLek9zBrLmBlT9Ty6T1VmIbRDxaqS9hFiRNgFsf1T6q3oHUQNbHqQXMwsxA2kbUAKLDpP0H4Daf056mKVMAAAAASUVORK5CYII=" alt="" style={{ opacity: 0, transition: 'opacity 500ms linear', objectFit: 'cover' }} />
+            <picture>
+              <source type="image/avif" srcSet="/static/3b4d6e8f44baf7e6d7a0ed4b3e4d8d39/9aa63/halcyon.avif 175w, /static/3b4d6e8f44baf7e6d7a0ed4b3e4d8d39/e6db6/halcyon.avif 350w, /static/3b4d6e8f44baf7e6d7a0ed4b3e4d8d39/f47db/halcyon.avif 700w" sizes="(min-width: 700px) 700px, 100vw" />
+              <source type="image/webp" srcSet="/static/3b4d6e8f44baf7e6d7a0ed4b3e4d8d39/240e7/halcyon.webp 175w, /static/3b4d6e8f44baf7e6d7a0ed4b3e4d8d39/faefe/halcyon.webp 350w, /static/3b4d6e8f44baf7e6d7a0ed4b3e4d8d39/d5dc4/halcyon.webp 700w" sizes="(min-width: 700px) 700px, 100vw" />
+              <img width="700" height="438" data-main-image="" sizes="(min-width: 700px) 700px, 100vw" decoding="async" src="/static/3b4d6e8f44baf7e6d7a0ed4b3e4d8d39/d2194/halcyon.png" srcSet="/static/3b4d6e8f44baf7e6d7a0ed4b3e4d8d39/cebcc/halcyon.png 175w, /static/3b4d6e8f44baf7e6d7a0ed4b3e4d8d39/272cf/halcyon.png 350w, /static/3b4d6e8f44baf7e6d7a0ed4b3e4d8d39/d2194/halcyon.png 700w" alt="Halcyon Theme" style={{ objectFit: 'cover', opacity: 1 }} />
+            </picture>
+            <noscript></noscript>
+          </div>
+        </a>
+      </div>
+    </li>
+  );
+};
--- portfolio-Fran/src/pages/Work.jsx
+++ portfolio-Fran/src/pages/Work.jsx
@@ -1,5 +1,6 @@
 import './Pages.css';
 import { ProjectCard } from '../components/Components';
+import { FeaturedProjectCard } from '../components/Components';
 
 const Work = () => {
     const projects = [
@@ -21,26 +22,35 @@ const Work = () => {
     ];
 
     return (
-        <section className="Flex" id="work">
-            <div id="work">
-                <div className="numbered-heading">
-                    <h2 className="title-section">Some Things I’ve Built</h2>
-                </div>
-                <ul className="projects-grid">
-                    {projects.map((project, index) => (
-                        <ProjectCard
-                            key={index}
-                            title={project.title}
-                            description={project.description}
-                            technologies={project.technologies}
-                            githubLink={project.githubLink}
-                            projectLink={project.projectLink}
-                        />
-                    ))}
-                </ul>
-            </div>
+        <section className="Flex experience" id="work">
+        <div id="work">
+          <div className="numbered-heading">
+            <h2 className="title-section">Some Things I’ve Built</h2>
+          </div>
+          <ul className="projects-feautured">
+          <FeaturedProjectCard
+              title="Halcyon Theme"
+              description="A minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm, and more. Available on Visual Studio Marketplace, Package Control, Atom Package Manager, and npm."
+              technologies={['VS Code', 'Sublime Text', 'Atom', 'iTerm2', 'Hyper']}
+              githubLink="https://github.com/bchiang7/halcyon-site"
+              projectLink="https://halcyon-theme.netlify.com/"
+            />
+          </ul>
+          <ul className="projects-grid">
+            {projects.map((project, index) => (
+              <ProjectCard
+                key={index}
+                title={project.title}
+                description={project.description}
+                technologies={project.technologies}
+                githubLink={project.githubLink}
+                projectLink={project.projectLink}
+              />
+            ))}
+          </ul>
+        </div>
         </section>
-    );
-};
-
-export default Work;
+      );
+    };
+    
+    export default Work;
